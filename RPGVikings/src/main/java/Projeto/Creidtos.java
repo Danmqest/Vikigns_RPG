afb3553e4e144cc695cdc989be689e2f32c45ea8
genivaldo.aanjos <gda_anjos@hotmail.com.br>
2022-05-23 19:31:24 -0300

    Merge remote-tracking branch 'origin/main'

---
+++ RPGVikings/src/main/java/Projeto/Creidtos.java
@@ -0,0 +1,24 @@
+package Projeto;
+import java.awt.*;
+import javax.swing.*;
+
+public class Creidtos {
+    public static void main(String[] args) {
+
+        JLabel myLabel = new JLabel(" ");
+        String creditos = "( Grupo 8 )";
+
+        System.out.println("Nome dos integrantes  \n\n\n" +
+                "" +
+                        " Danilio Maxinimiano \t\n\n\n" +
+                        " Karina Silva \t\n\n\n" +
+                        " Elveis Andre \t\n\n\n" +
+                "" +
+                "" +
+                "\n\n\t" +
+                "Curso Ministrado PROJETO INTEGRADOR DESENVOLVIMENTO DE LÓGICA:");
+    }
+
+
+
+}
